2f2ec1165e93588d07f5525896ad054e47667e4d
peetzweg/ <phil.czek@gmail.com>
2023-11-27 14:32:13 +0100

    adds a `theme-provider` to toggle dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useTheme } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
 
 function App() {
+  const { setTheme, theme } = useTheme();
   return (
     <div className="h-screen v-screen flex flex-col justify-center items-center">
       <h1 className="text-2xl">Hello Proof of Ink</h1>
-      <Button>Toggle Theme</Button>
+      <Button
+        onClick={() => {
+          setTheme(theme === "dark" ? "light" : "dark");
+        }}
+      >
+        Toggle Theme
+      </Button>
     </div>
   );
 }
