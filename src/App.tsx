bf9136cafe3c274e36ed6ab9fba33f465743db41
peetzweg/ <phil.czek@gmail.com>
2023-11-27 14:40:53 +0100

    adds tailwindcss prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "./components/ui/button"
 function App() {
   const { setTheme, theme } = useTheme()
   return (
-    <div className="h-screen v-screen flex flex-col justify-center items-center">
+    <div className="v-screen flex h-screen flex-col items-center justify-center">
       <h1 className="text-2xl">Hello Proof of Ink</h1>
       <Button
         onClick={() => {
