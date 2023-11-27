8d308bd96920f7186b4113dc127b9ca208b61cf8
peetzweg/ <phil.czek@gmail.com>
2023-11-27 14:39:17 +0100

    chore: install prettier 💄

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useTheme } from "./components/theme-provider";
-import { Button } from "./components/ui/button";
+import { useTheme } from "./components/theme-provider"
+import { Button } from "./components/ui/button"
 
 function App() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme, theme } = useTheme()
   return (
     <div className="h-screen v-screen flex flex-col justify-center items-center">
       <h1 className="text-2xl">Hello Proof of Ink</h1>
       <Button
         onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
+          setTheme(theme === "dark" ? "light" : "dark")
         }}
       >
         Toggle Theme
       </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
