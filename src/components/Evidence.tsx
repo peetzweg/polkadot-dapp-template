ec7ad50ececd681631d22099423108ac129847ef
peetzweg/ <phil.czek@gmail.com>
2024-01-12 17:08:04 +0100

    starts handling user selected file instead of ignoring it

--- src/components/Evidence.tsx
+++ src/components/Evidence.tsx
@@ -1,12 +1,14 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { ShadowInnerIcon, UploadIcon } from "@radix-ui/react-icons"
 import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { useCallback, useMemo } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
 import * as z from "zod"
 import { useExtrinsic } from "../lib/useExtrinsic.js"
 import { cn } from "../lib/utils.js"
 import { useApi } from "../providers/api-provider.js"
+import { blake2AsU8a } from "@polkadot/util-crypto"
+import { u8aToHex } from "@polkadot/util"
 import {
   QUERY_KEY as QUERY_KEY_CANDIDATE_STATE,
   useQueryCandidateState,
@@ -22,20 +24,22 @@ import {
   FormMessage,
 } from "./ui/form.js"
 import { Input } from "./ui/input.js"
+import { AspectRatio } from "./ui/aspect-ratio.js"
 
-import "../interfaces/bulletin/augment-api.js"
+// import "../interfaces/bulletin/augment-api.js"
 // import "../interfaces/bulletin/augment-types"
 interface EvidenceProps {
   className?: string
 }
 
 const formSchema = z.object({
-  evidence: z.any(),
+  evidence: z.instanceof(FileList),
 })
 
 export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
   const { api } = useApi()
   const { Bulletin } = useChain()
+  const [file, setFile] = useState<File | undefined>(undefined)
 
   const queryClient = useQueryClient()
   const form = useForm<z.infer<typeof formSchema>>({
@@ -46,7 +50,9 @@ export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
 
   const { data, isLoading } = useQuery({
     queryKey: ["Bulletin", "getBulletin"],
-    queryFn: () => Bulletin.query.transactionStorage.authorizations.entries(),
+    queryFn: () => {
+      return Bulletin.query.transactionStorage.authorizations.entries()
+    },
   })
   console.log({ data, isLoading })
 
@@ -55,13 +61,30 @@ export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
   )
 
   const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = useCallback(
-    ({ evidence }) => {
-      // eslint-disable-next-line no-console
-      console.log({ uploaded: evidence })
-      const DEADBEEF =
-        "0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF"
+    async ({ evidence }) => {
+      if (evidence?.[0] === undefined) {
+        throw new Error("No evidence provided")
+      }
 
-      return submitEvidence([DEADBEEF], {
+      // Task: Check if size is in limits
+      // TODO: use value from Bulletin Allowance here
+      if (evidence[0].size > 1e10) {
+        throw new Error("File too large")
+      }
+
+      console.log({ evidence: evidence[0], type: evidence[0].type })
+
+      // Task: get blake2 of file
+      const buffer = await evidence[0].arrayBuffer()
+      const bytes = new Uint8Array(buffer)
+
+      // TODO: maybe this needs to be done in a service worker for bigger files?
+      const blake2HashOfFile = u8aToHex(blake2AsU8a(bytes))
+
+      // Task: Submit evidence bytes to bulletin chain
+
+      // Task: submit hash of evidence to ProofOfInk
+      return submitEvidence([blake2HashOfFile], {
         onSuccess: () => {
           void queryClient.invalidateQueries({
             queryKey: QUERY_KEY_CANDIDATE_STATE,
@@ -71,6 +94,13 @@ export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
     },
     [queryClient, submitEvidence],
   )
+
+  const onChange = useCallback((event: React.FormEvent<HTMLInputElement>) => {
+    const files = event.currentTarget.files
+    if (files?.[0]) {
+      setFile(files[0])
+    }
+  }, [])
 
   const { active, done } = useMemo(() => {
     return {
@@ -104,21 +134,35 @@ export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
           <code>ProofOfInk::submit_evidence(hash)</code>
         </div>
 
-        <Form {...form}>
-          <form
-            // TODO type error of react-hook-forms?
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
+        {file && (
+          <AspectRatio
+            ratio={1 / 1}
+            className="flex items-center justify-center overflow-hidden"
           >
-            <div className="flex h-32 items-center justify-center gap-2 rounded border border-dashed">
+            <img src={URL.createObjectURL(file)} />
+          </AspectRatio>
+        )}
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <input
+            {...form.register("evidence")}
+            onChange={onChange}
+            accept="image/*"
+            className="cursor-pointer"
+            type="file"
+          />
+
+          {/* <div className="flex h-32 items-center justify-center gap-2 rounded border border-dashed">
               <FormField
                 control={form.control}
                 name="evidence"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input
+                      <input
                         className="cursor-pointer"
                         type="file"
                         {...field}
@@ -128,24 +172,23 @@ export const Evidence: React.FC<EvidenceProps> = ({ className }) => {
                   </FormItem>
                 )}
               />
-            </div>
-            <Button
-              type="submit"
-              disabled={
-                form.formState.isLoading ||
-                !form.formState.isValid ||
-                form.formState.isSubmitting
-              }
-            >
-              Submit{" "}
-              {form.formState.isSubmitting ? (
-                <ShadowInnerIcon className="ml-2 animate-spin" />
-              ) : (
-                <UploadIcon className="ml-2" />
-              )}
-            </Button>
-          </form>
-        </Form>
+            </div> */}
+          <Button
+            type="submit"
+            // disabled={
+            //   form.formState.isLoading ||
+            //   !form.formState.isValid ||
+            //   form.formState.isSubmitting
+            // }
+          >
+            Submit{" "}
+            {form.formState.isSubmitting ? (
+              <ShadowInnerIcon className="ml-2 animate-spin" />
+            ) : (
+              <UploadIcon className="ml-2" />
+            )}
+          </Button>
+        </form>
       </div>
     </div>
   )
